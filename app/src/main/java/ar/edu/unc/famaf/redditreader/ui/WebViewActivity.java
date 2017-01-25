ece04087d2a7d9ad24fd59e41cfe724d9a2c2293
Elías Gasparini <gasparinielias@gmail.com>
2017-01-25 00:20:23 -0300

    Arreglado JavaScript y navegación en la WebView

--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/WebViewActivity.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/WebViewActivity.java
@@ -4,20 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by mono on 27/11/16.
  */
 
 public class WebViewActivity extends AppCompatActivity {
+    WebView mWebView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
-        WebView webView = new WebView(this);
-        setContentView(webView);
-        webView.loadUrl(url);
+
+        mWebView = new WebView(this);
+        WebSettings webSettings = mWebView.getSettings();
+
+        // Enable JavaScript
+        webSettings.setJavaScriptEnabled(true);
+
+        // Handle page navigation
+        mWebView.setWebViewClient(new WebViewClient());
+
+        setContentView(mWebView);
+        mWebView.loadUrl(url);
     }
+
 }
